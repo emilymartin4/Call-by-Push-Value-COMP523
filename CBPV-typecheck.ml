ff2a6d294b13dc5bb3542928aaf8d0553c0efe19
L Denney <156242030+loladenney@users.noreply.github.com>
2025-03-31 11:13:16 -0400

    I didn't change anything other than comments

--- CBPV-typecheck.ml
+++ CBPV-typecheck.ml
@@ -7,14 +7,14 @@ type tpV =
 | Unit
 | Bool
 | Num
-| U of tpC             (* type of thUnk x *)
+| U of tpC              (* type of thUnk x *)
 | ECross of tpV * tpV   (* pattern matched product p43 *)
 | Sum of tpV * tpV
 and 
 tpC = 
-| F of tpV             (* type of Force x *)
+| F of tpV              (* type of Force x *)
 | Arrow of tpV * tpC
-| LCross of tpC * tpC  
+| LCross of tpC * tpC   (* projection eliminated product*)
 
 
 
@@ -38,7 +38,7 @@ type tm =
 | Inr of tm
 | Case of tm * string * tm * string * tm
 | Lam of string * tpV * tm                  (* also called pop *)
-| App of tm * tm                             (* also called push *)
+| App of tm * tm                            (* also called push *)
 | Produce of tm
 | Force of tm
 | Thunk of tm
@@ -53,7 +53,8 @@ exception TypeBad of string
 (* p38 *)
 (* TODO: refactor with a type of for values and a type of for computations following page 44*)
 (* typechecker: typing rules, use options to catch failure... (then later extend with helpful stuff??) *)
-(*todo: enforce anything added to environment is a value type*)
+(*todo: enforce anything added to context is a value type*)
+
 let rec typeofV (ctx : ctx) (t : tm) : tpV = match t with
   | Var x -> (match find_opt (fun y -> fst y = x ) ctx with 
     | None -> raise (TypeBad ("variable " ^ x ^ " is free"))
@@ -70,19 +71,19 @@ let rec typeofV (ctx : ctx) (t : tm) : tpV = match t with
   | Inr t ->(match typeofV ctx t with 
     | Sum (a1, a2) -> a2
     | _ -> raise (TypeBad "Inr isnt working:("))
-  | Case (t,x,t1,y,t2) -> (match typeofV ctx t with        (* TODO: check that a is a value becaore putting in context!! (helper function that uses list module) *)
+  | Case (t,x,t1,y,t2) -> (match typeofV ctx t with        (* TODO: check that a is a value becaore putting in context!! (helper function that uses list module?) *)
     | Sum (a1, a2) -> 
         let (b1,b2) = typeofV ((x, a1)::ctx) t1, typeofV ((y, a2)::ctx) t2 in 
         if b1 = b2 then b1 else raise (TypeBad "branches of sym type destructor don't type check")
     | _ -> raise (TypeBad "Case trying to match on non-sum type"))  (* TODO: need to define equality for types? *)
   | EagerPair (t1, t2) -> (let (x,y) = typeofV ctx t1, typeofV ctx t2 in ECross (x,y))  
   | Match (t1, x, y, t2) -> (match typeofV ctx t with 
-    | ECross (v1,v2) -> typeofV ([(x, v1);(y, v2)]  @ ctx) t2                               (* TODO: check that a is a value becaore putting in context!! (helper function that uses list module) *)
+    | ECross (v1,v2) -> typeofV ([(x, v1);(y, v2)]  @ ctx) t2                               (* TODO: check that a is a value becaore putting in context!! (helper function that uses list module?) *)
     | _ -> raise (TypeBad ("match on eager pair bad")))
   | _ ->  raise (TypeBad ("supposed to be a value, but its not"))
 
 and typeofC  (ctx : ctx) (t : tm) : tpC = match t with
-  | Lam (x,a,t) -> Arrow( a, typeofC ((x,a)::ctx) t)                      (* TODO: check that a is a value becaore putting in context!! (helper function that uses list module) *)
+  | Lam (x,a,t) -> Arrow( a, typeofC ((x,a)::ctx) t)                      (* TODO: check that a is a value becaore putting in context!! (helper function that uses list module?) *)
   | LetIn (x, t1, t2) -> (let a = typeofV ctx t1 in                   (* type annotation needed? *)
       typeofC ((x, a) :: ctx) t2 )
   | Produce t -> F (typeofV ctx t)
